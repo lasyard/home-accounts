070566186f508beae9719d64a270accf6d12c4ab
Lasy <lasyard@yeah.net>
2023-06-27 13:45:55 +0800

    Refactor cmake files.

--- src/gui/Common.h
+++ src/gui/Common.h
@@ -2,6 +2,6 @@
 #define _GUI_COMMON_H_
 
 #define DECLARE_TM()      static const wxChar *const TM;
-#define IMPLEMENT_TM(cls) const wxChar *const cls::TM = _(#cls);
+#define IMPLEMENT_TM(cls) const wxChar *const cls::TM = wxS(#cls);
 
 #endif /* _GUI_COMMON_H_ */
